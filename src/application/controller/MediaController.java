5c16a88f7e19b85e0093c0a52664a2f7387c7498
AmielSTongco <amiel_s_tongco@dlsu.edu.ph>
2026-07-29 15:28:21 +0800

    Added additional buttons on navigation bar

--- src/application/controller/MediaController.java
+++ src/application/controller/MediaController.java
@@ -22,8 +22,11 @@ import javafx.util.Duration;
 public class MediaController {
 
 	// For Expandable Navigation Bar
-	private static final int deltaXNavButton1 = 10;
-	private static final int deltaXNavButton2 = -10;
+	private static final int deltaXEditButton   = 40;
+	private static final int deltaXRemoveButton = 20;
+	private static final int deltaXDeleteButton = 0;
+	private static final int deltaXBackButton   = -20;
+	private static final int deltaXHomeButton   = -40;
 	
 	private Rectangle clipRect;
 	 
@@ -54,13 +57,19 @@ public class MediaController {
     private ImageView profileAvatar;
 
     @FXML
-    private Button navButton1;
+    private Button editButton;
 
     @FXML
-    private Button navButton2;
+    private Button removeButton;
     
     @FXML
-    private Button navButton3;
+    private Button deleteButton;
+    
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private Button homeButton;
 
     	@FXML 
     	private Label avgPlaytimeLabel;
@@ -120,8 +129,11 @@ public class MediaController {
         
         clipRect = new Rectangle();
 		clipRect.setWidth(extendableNavigationPane.getPrefWidth());
-		setIcon(navButton1, "/resources/application/images/icons/plus-svgrepo-com.png");
-		setIcon(navButton2, "/resources/application/images/icons/back-reply-svgrepo-com.png");
+		setIcon(editButton, "/resources/application/images/icons/pencil-svgrepo-com.png");
+		setIcon(removeButton, "/resources/application/images/icons/minus-svgrepo-com.png");
+		setIcon(deleteButton, "/resources/application/images/icons/trash-can-svgrepo-com.png");
+		setIcon(backButton, "/resources/application/images/icons/back-reply-svgrepo-com.png");
+		setIcon(homeButton, "/resources/application/images/icons/home-icon-svgrepo-com.png");
 		hidePane();
     	
         // Create a rounded rectangle for media image
@@ -133,7 +145,6 @@ public class MediaController {
         
         DropShadow shadow = new DropShadow();
         shadow.setRadius(10);
-        //shadow.setOffsetX(5);
         shadow.setOffsetY(5);
         shadow.setColor(Color.color(0, 0, 0, 0.4));
         
@@ -175,6 +186,12 @@ public class MediaController {
     
     @FXML
 	private void showPane() { 
+	    	editButton.setText("Update Details");
+	    	removeButton.setText("Remove From Playlist");
+	    	deleteButton.setText("Delete Media");
+	    	backButton.setText("Back");
+	    	homeButton.setText("Home");
+    	
 		// Animation for showing the pane completely
 		Timeline timelineDown = new Timeline();
  
@@ -183,22 +200,38 @@ public class MediaController {
 		final KeyValue kvDwn3 = new KeyValue(extendableNavigationPane.translateYProperty(), 0);
 		final KeyFrame kfDwn = new KeyFrame(Duration.millis(100), createBouncingEffect(extendableNavigationPane.getHeight()), kvDwn1, kvDwn2, kvDwn3);
  
-		// Animation for moving button 1
-		final KeyValue kvB1 = new KeyValue(navButton1.translateXProperty(), -deltaXNavButton1);
-		final KeyFrame kfB1 = new KeyFrame(Duration.millis(200), kvB1);
- 
-		// Animation for moving button 2
-		final KeyValue kvB2 = new KeyValue(navButton2.translateXProperty(), -deltaXNavButton2);
-		final KeyFrame kfB2 = new KeyFrame(Duration.millis(200), kvB2);
- 
-		navButton1.setText("Add Playlist");
-		navButton2.setText("Back");
-		timelineDown.getKeyFrames().addAll(kfDwn, kfB1, kfB2);
+		// Animation for moving Edit button
+		final KeyValue kvEdit = new KeyValue(editButton.translateXProperty(), -deltaXEditButton);
+		final KeyFrame kfEdit = new KeyFrame(Duration.millis(200), kvEdit);
+
+		// Animation for moving Remove button
+		final KeyValue kvRemove = new KeyValue(removeButton.translateXProperty(), -deltaXRemoveButton);
+		final KeyFrame kfRemove = new KeyFrame(Duration.millis(200), kvRemove);
+
+		// Animation for moving Delete button
+		final KeyValue kvDelete = new KeyValue(deleteButton.translateXProperty(), -deltaXDeleteButton);
+		final KeyFrame kfDelete = new KeyFrame(Duration.millis(200), kvDelete);
+
+		// Animation for moving Back button
+		final KeyValue kvBack = new KeyValue(backButton.translateXProperty(), -deltaXBackButton);
+		final KeyFrame kfBack = new KeyFrame(Duration.millis(200), kvBack);
+
+		// Animation for moving Home button
+		final KeyValue kvHome = new KeyValue(homeButton.translateXProperty(), -deltaXHomeButton);
+		final KeyFrame kfHome = new KeyFrame(Duration.millis(200), kvHome);
+
+		timelineDown.getKeyFrames().addAll(kfDwn, kfEdit, kfRemove, kfDelete, kfBack, kfHome);
 		timelineDown.play();
 	}
  
 	@FXML
-	private void hidePane() { 
+	private void hidePane() {
+		editButton.setText(null);
+		removeButton.setText(null);
+		deleteButton.setText(null);
+		backButton.setText(null);
+		homeButton.setText(null);
+		
 		// Animation for hiding the pane..
 		Timeline timelineUp = new Timeline();
  
@@ -206,36 +239,71 @@ public class MediaController {
 		final KeyValue kvUp2 = new KeyValue(extendableNavigationPane.translateYProperty(), 10);
 		final KeyFrame kfUp = new KeyFrame(Duration.millis(200), kvUp1, kvUp2);
  
-		// Animation for moving button 1
-		final KeyValue kvB1 = new KeyValue(navButton1.translateXProperty(), deltaXNavButton1);
-		final KeyFrame kfB1 = new KeyFrame(Duration.millis(200), kvB1);
- 
-		final KeyValue kvB2 = new KeyValue(navButton2.translateXProperty(), deltaXNavButton2);
-		final KeyFrame kfB2 = new KeyFrame(Duration.millis(200), kvB2);
- 
-		navButton1.setText(null);
-		navButton2.setText(null);
-		timelineUp.getKeyFrames().addAll(kfUp, kfB1, kfB2);
+		// Animation for moving Edit button
+		final KeyValue kvEdit = new KeyValue(editButton.translateXProperty(), deltaXEditButton);
+		final KeyFrame kfEdit = new KeyFrame(Duration.millis(200), kvEdit);
+
+		// Animation for moving Remove button
+		final KeyValue kvRemove = new KeyValue(removeButton.translateXProperty(), deltaXRemoveButton);
+		final KeyFrame kfRemove = new KeyFrame(Duration.millis(200), kvRemove);
+
+		// Animation for moving Delete button
+		final KeyValue kvDelete = new KeyValue(deleteButton.translateXProperty(), deltaXDeleteButton);
+		final KeyFrame kfDelete = new KeyFrame(Duration.millis(200), kvDelete);
+		
+		// Animation for moving Back button
+		final KeyValue kvBack = new KeyValue(backButton.translateXProperty(), deltaXBackButton);
+		final KeyFrame kfBack = new KeyFrame(Duration.millis(200), kvBack);
+
+		// Animation for moving Home button
+		final KeyValue kvHome = new KeyValue(homeButton.translateXProperty(), deltaXHomeButton);
+		final KeyFrame kfHome = new KeyFrame(Duration.millis(200), kvHome);
+
+		timelineUp.getKeyFrames().addAll(kfUp, kfEdit, kfRemove, kfDelete, kfBack, kfHome);
 		timelineUp.play();
 	}
  
 	@FXML
-	private void selectPane1() {
+	private void toggleEdit() {
 		System.out.println("Selecting pane 1");
 		deselectAllPanes();
-		navButton1.setEffect(dropShadowForSelectedPane);
+		editButton.setEffect(dropShadowForSelectedPane);
 	}
  
 	@FXML
-	private void selectPane2() {
+	private void removeMedia() {
 		System.out.println("Selecting pane 2");
 		deselectAllPanes();
-		navButton2.setEffect(dropShadowForSelectedPane);
+		removeButton.setEffect(dropShadowForSelectedPane);
+	}
+	
+	@FXML
+	private void deleteMedia() {
+		System.out.println("Selecting pane 3");
+		deselectAllPanes();
+		deleteButton.setEffect(dropShadowForSelectedPane);
+	}
+	
+	@FXML
+	private void goToBack() {
+		System.out.println("Selecting pane 4");
+		deselectAllPanes();
+		backButton.setEffect(dropShadowForSelectedPane);
+	}
+	
+	@FXML
+	private void goToHome() {
+		System.out.println("Selecting pane 5");
+		deselectAllPanes();
+		homeButton.setEffect(dropShadowForSelectedPane);
 	}
  
 	private void deselectAllPanes() {
-		navButton1.setEffect(null);
-		navButton2.setEffect(null);
+		editButton.setEffect(null);
+		removeButton.setEffect(null);
+		deleteButton.setEffect(null);
+		backButton.setEffect(null);
+		homeButton.setEffect(null);
 	}
  
 	private EventHandler<ActionEvent> createBouncingEffect(double height) {
